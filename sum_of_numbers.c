972073e38639d064774b39e45363bbfef6b5b144
AjayYadavAi <proajayyadav0525@gmail.com>
2020-01-19 13:27:06 +0530

    modification

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -3,15 +3,19 @@ int main(){
 	int n,i,sum;
 	printf("\nEnter number to get the sum : ");
 	scanf("%d",&n);
-	sum = n*(n+1);
-	sum = sum/2;
-	printf("\nSum of %d is : %d",n,sum);
-	/* 
-	OR You can get the sum with loop
-	*/
-	while(n != 0){
-		sum += n;
-		n--;
+	if(n > 1){
+		sum = n*(n+1);
+		sum = sum/2;
+		printf("\nSum of %d is : %d",n,sum);
+		/* 
+		OR You can get the sum with loop
+		*/
+		while(n != 0){
+			sum += n;
+			n--;
+		}
+		printf("\nSum is : %d",sum);
+	}else{
+		printf("\nPlease enter number greater than 1");
 	}
-	printf("\nSum is : %d",sum);
 }
